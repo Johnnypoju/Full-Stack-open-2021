cfcd65a26c0d6b79a8bbb64f56c521b348cced58
Joonas Sumusalo <sumusalo.joonas@gmail.com>
2021-08-20 11:54:15 +0300

    exercise kurssitiedot final push

--- part1/kurssitiedot/src/App.js
+++ part1/kurssitiedot/src/App.js
@@ -11,17 +11,48 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
+      <Header course={course}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
+    </div>
+    
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
       <p>
-        {part1} {exercises1}
+        Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
       </p>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
       <p>
-        {part2} {exercises2}
+        {props.part} {props.exercises}
       </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
